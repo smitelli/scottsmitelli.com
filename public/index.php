d3d0c0c38189b6f0e4c0171fdb06fe06279bc106
Scott Smitelli <scott@smitelli.com>
2017-08-13 17:58:25 -0400

    Can't do define() from php-fpm, or I'm not looking hard enough

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
   ini_set('display_errors', 0);
   date_default_timezone_set('America/New_York');
 
+  define('CONTENT_DIR', dirname(__FILE__) . '/..');
+
   require dirname(__FILE__) . '/../docroute/bootstrap.php';
 
 ?>
