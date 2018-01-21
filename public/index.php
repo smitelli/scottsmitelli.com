c4f0eaa2bd92bbd663431a58a2dc2d755123e27a
Scott Smitelli <scott@smitelli.com>
2018-01-21 17:42:30 -0500

    My site, my lazy hosting config constants

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
   date_default_timezone_set('America/New_York');
 
   define('CONTENT_DIR', dirname(__FILE__) . '/..');
+  define('COMPILE_DIR', '/var/opt/website/scottsmitelli.com/compile');
 
   require dirname(__FILE__) . '/../docroute/bootstrap.php';
 
